6039d3cc0f61281e1887aebe9f5eff3ac260b1c5
hsdyyd <yidong06@sina.com>
2019-01-08 22:44:58 +0800

    add class

---
+++ src/main/java/com/train/test/TestOther.java
@@ -0,0 +1,11 @@
+package com.train.test;
+
+/**
+ * @author yidong
+ * @create 2019-01-08 22:42
+ */
+public class TestOther {
+    public void test(){
+        System.out.println("=======");
+    }
+}
